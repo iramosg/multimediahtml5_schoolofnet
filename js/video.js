f1881653a8414bdb7970e5320bb705bd0f7730a1
Igor Ramos <igor.ramos@live.com>
2018-02-10 17:23:48 -0200

    adicionando volume, mudo e full screen

--- js/video.js
+++ js/video.js
@@ -7,14 +7,47 @@ function play(){
     video.play();
 }
 
-function pause() {
+function pause(){
     var video = getVideo();
     video.pause();
 }
 
-function stop() {
+function stop(){
     var video = getVideo();
     // video.pause();
     // video.currentTime = 0;
     video.load();
+}
+
+function volume(vol){
+    var volume = document.getElementById("volume").value;
+    if(volume === "100"){
+        volume = 1;
+    } else {
+        volume = parseFloat("0." + volume).toFixed(1);
+    }
+
+    var video = getVideo();
+    video.volume = volume;
+}
+
+function muted(){
+    var video = getVideo();
+    var muted = document.getElementById("muted");
+    if(video.muted){
+        video.muted = false;
+        muted.style.color = "black";
+    } else {
+        video.muted = true;
+        muted.style.color = "red";      
+    }
+}
+
+function full() {
+    var video = getVideo();
+    video.webkitRequestFullScreen(); //Só está funcionando no Google Chrome e Opera
+}
+
+getVideo().onplay = function(){
+    console.log("deu play");
 }
